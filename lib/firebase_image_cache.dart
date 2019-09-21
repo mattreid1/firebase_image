8da484eb2cbbcf8a59ac740033ee74c4bac1389d
Matt Reid <matthewreid2000@gmail.com>
2019-09-21 13:36:19 +0100

    Returns ImageProvider.

--- lib/firebase_image_cache.dart
+++ lib/firebase_image_cache.dart
@@ -1,15 +1,20 @@
 library firebase_image_cache;
 
+import 'dart:ui';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class FirebaseImage {
+class FirebaseImage extends ImageProvider<FirebaseImage> {
   /// The URI of the image
   final String location;
 
   // Should the image be cached (optional)
   final bool shouldCache;
+
+  /// The scale to display the image at (optional)
+  final double scale;
 
   /// The maximum size in bytes to be allocated on the device for the image (optional)
   final int maxSizeBytes;
@@ -26,17 +31,29 @@ class FirebaseImage {
   const FirebaseImage(
     this.location, {
     this.shouldCache = true,
+    this.scale = 1.0,
     this.maxSizeBytes = 2500 * 1000, // 2.5MB
     FirebaseApp firebaseApp,
   }) : _firebaseApp = firebaseApp;
 
-  Future<Image> fetch() async {
+  Future<Codec> _fetch() async {
     final uri = Uri.parse(location);
 
     FirebaseStorage storage = FirebaseStorage(
-        app: _firebaseApp, storageBucket: "${uri.scheme}://${uri.authority}");
+        app: _firebaseApp, storageBucket: '${uri.scheme}://${uri.authority}');
     final bytes = await storage.ref().child(uri.path).getData(maxSizeBytes);
 
-    return Image.memory(bytes);
+    return await PaintingBinding.instance.instantiateImageCodec(bytes);
+  }
+
+  @override
+  Future<FirebaseImage> obtainKey(ImageConfiguration configuration) {
+    return SynchronousFuture<FirebaseImage>(this);
+  }
+
+  @override
+  ImageStreamCompleter load(FirebaseImage key) {
+    return MultiFrameImageStreamCompleter(
+        codec: key._fetch(), scale: key.scale);
   }
 }
