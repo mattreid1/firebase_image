0116edfecea2886bb5925004dc4bf0e34c8ceea0
Matt Reid <matthewreid2000@gmail.com>
2019-09-21 13:17:09 +0100

    Basic implementation - can get bytes from bucket.

--- lib/firebase_image_cache.dart
+++ lib/firebase_image_cache.dart
@@ -5,5 +5,38 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class FirebaseImage {
+  /// The URI of the image
+  final String location;
 
+  // Should the image be cached (optional)
+  final bool shouldCache;
+
+  /// The maximum size in bytes to be allocated on the device for the image (optional)
+  final int maxSizeBytes;
+
+  /// The Firebase app to make the request from (optional)
+  final FirebaseApp _firebaseApp;
+
+  /// TODO: Add descriptions
+  ///
+  /// [location]
+  /// [shouldCache]
+  /// [maxSizeBytes]
+  /// [firebaseApp]
+  const FirebaseImage(
+    this.location, {
+    this.shouldCache = true,
+    this.maxSizeBytes = 2500 * 1000, // 2.5MB
+    FirebaseApp firebaseApp,
+  }) : _firebaseApp = firebaseApp;
+
+  Future<Image> fetch() async {
+    final uri = Uri.parse(location);
+
+    FirebaseStorage storage = FirebaseStorage(
+        app: _firebaseApp, storageBucket: "${uri.scheme}://${uri.authority}");
+    final bytes = await storage.ref().child(uri.path).getData(maxSizeBytes);
+
+    return Image.memory(bytes);
+  }
 }
